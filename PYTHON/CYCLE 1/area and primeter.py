13b2e2a4af4580446af5447f2a7baef75e444435
SHIVARAMA <145856726+shivarama02@users.noreply.github.com>
2024-11-29 15:10:14 +0530

    Update area and primeter.py

--- PYTHON/CYCLE 1/area and primeter.py
+++ PYTHON/CYCLE 1/area and primeter.py
@@ -1,8 +1,8 @@
 import math
 r = int(input("Enter the radius of a circle: "))
 
-area = math.pi*r*r
-perimeter = 2*math.pi*r
+a = math.pi*r*r
+p = 2*math.pi*r
 
 print(f"The area of the circle is {area}")
 print(f"The perimeter of the circle is {perimeter}")
