9466fab2ca070ea47e989b341221d94b60edc87a
shivarama02 <naikshivarama6@gmail.com>
2025-01-12 00:35:36 +0530

    atm.py modifed

--- Lab Practice/PYTHON/atm.py
+++ Lab Practice/PYTHON/atm.py
@@ -15,6 +15,7 @@ class Acount:
         self.acc_type=acc_type
         self.pin=pin
         self.balance=balance
+        self.transaction=None
     
     def Displaydetails(self):
         print("Account holder name: ",self.name)
@@ -34,6 +35,7 @@ class Acount:
             enter_pin=input("Enter your 4 digit pin:")
             if enter_pin==self.pin:
                 self.balance += amount
+                self.transaction = f"Deposited {amount}/-"
                 print(f"{amount} deposited to your acconut successfully!")
             else:
                 print("Invalid pin! Transaction failed.")
@@ -47,18 +49,32 @@ class Acount:
         elif self.acc_type=='savings' and self.balance-amount<self.MIN_SAVINGS :
             print("Savings account must contain a minimum balance of ",self.MIN_SAVINGS)
         elif amount>self.balance:
-            print("Insufficeint amonut!")
+            print("Insufficeint balance!")
         else:
             enter_pin=input("Enter your 4 digit pin: ")
             if enter_pin==self.pin:
                 self.balance -= amount
+                self.transaction = f"Withdrawn {amount}/-"
                 print(f"{amount} withdrawn from your account successfully!") 
             else:
                 print("Invalid pin! Transaction failed.")
+
+    def receipt(self):
+        print("------Transaction receipt------")
+        print("Acconut name: ",self.name)
+        print("Account number: ",self.acc_no)
+        print("Account type: ",self.acc_type)
+        print("Last transaction: ",self.transaction)
+        print("Remaining balance: ",self.balance,"/-")
+        print("Thank you for using our banking service!")
     
 print("--------Account creation--------")
 name=input("Enter your name: ")
-accno=int(input("Enter your account number: "))
+accno=input("Enter your 8-digit account number: ")
+while len(accno) != 8 or not accno.isdigit():
+    print("Invalid account number! Please enter an 8-digit account number.")
+    accno = input("Enter your account number: ")
+
 acctype=input("Enter your account type(savings/current): ").lower()
 
 while acctype not in ('savings','current'):
@@ -67,8 +83,8 @@ while acctype not in ('savings','current'):
 print("Account created successfully!!")
 
 pin = input("Enter 4-digit PIN number: ")
-while len(pin)!=4 and pin.isdigit():
-    print("Pin number must contain 4 digit.")
+while len(pin)!=4 or not pin.isdigit():
+    print("Pin number must contain 4-digit.")
     pin = input("Enter a 4-digit PIN number: ")
 print("Pin generated successfully!!")
 
@@ -79,14 +95,15 @@ while(True):
     print("1.Check account details\n2.Check balance\n3.Deposite\n4.Withdraw\n5.Exit")
     op=int(input("Enter a option: "))
     if op==1:
-        print(account.Displaydetails())
+        account.Displaydetails()
     elif op==2:
-        print(account.Displaybalance())
+        account.Displaybalance()
     elif op==3:
         account.deposite()
     elif op==4:
         account.withdraw()
     elif op==5:
+        account.receipt()
         break
     else:
         print("Invalid option!")
